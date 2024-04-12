37886d95d0b284b5335d0acc18a9d8c441bb6d80
gsjoy24 <gour.joy24@gmail.com>
2024-04-12 21:03:13 +0600

    Update user2 table in alter.sql

--- alter.sql
+++ alter.sql
@@ -15,3 +15,19 @@ ALTER TABLE user2
    -- changing type 
 ALTER TABLE user2
    ALTER COLUMN user_name type VARCHAR(100);
+
+-- setting not null constraint to user_age
+ALTER TABLE user2
+   ALTER COLUMN user_age SET NOT NULL
+
+-- removing not null constraint to user_age
+ALTER TABLE user2
+   ALTER COLUMN user_age DROP NOT NULL;
+
+   -- adding unique constraint to user_age 
+ALTER TABLE user2
+   ADD CONSTRAINT unique_user2_user_age UNIQUE (user_age);
+
+-- removing unique constraint to user_age
+ALTER TABLE user2
+   DROP CONSTRAINT unique_user2_user_age;
